276bb39445871d59ba0ea1fd3c43bceaab6eb171
siddhant.kumar@bizzydesk.com <siddhant.singh326@gmail.com>
2024-07-09 22:21:22 +0530

    cctv-page

---
+++ src/components/BookingFormComponent.vue
@@ -0,0 +1,58 @@
+<template>
+<v-card class="pa-4 bg-white">
+    <v-card-title class="headline mb-5">Book Free Consulation Call</v-card-title>
+    <v-form>
+        <v-row>
+            <v-col cols="12" md="6">
+                <v-text-field label="First name" variant="outlined" hint="John"></v-text-field>
+            </v-col>
+            <v-col cols="12" md="6">
+                <v-text-field label="Last name" variant="outlined" hint="Doe"></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12">
+                <v-text-field label="Email" variant="outlined" hint="johndoe@mail.com"></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12">
+                <v-text-field label="Phone number" variant="outlined"
+                    hint="(647) 555-876"></v-text-field>
+            </v-col>
+        </v-row>
+        <v-btn color="primary" class="mt-4">Book Now</v-btn>
+    </v-form>
+    <v-card-text class="subheading mt-5">
+        Rest assured, quality service comes as standard:
+        <v-row>
+            <v-col cols="6">
+                <v-list  class="bg-white text">
+                  <v-list-item class="d-flex align-center">
+                    <v-icon color="black" size="20" class="mdi mdi-check"></v-icon>
+                    <span class="ml-2">Extended Warranties</span>
+                  </v-list-item>
+                  <v-list-item class="d-flex align-center">
+                    <v-icon color="black" size="20" class="mdi mdi-check"></v-icon>
+                    <span class="ml-2">Highly accredited</span>
+                  </v-list-item>
+                </v-list>
+              </v-col>
+              <v-col cols="6">
+                <v-list class="bg-white text">
+                  <v-list-item class="d-flex align-center">
+                    <v-icon color="black" size="20" class="mdi mdi-check"></v-icon>
+                    <span class="ml-2">Free estimates</span>
+                  </v-list-item>
+                  <v-list-item class="d-flex align-center">
+                    <v-icon color="black" size="20" class="mdi mdi-check"></v-icon>
+                    <span class="ml-2">100% satisfaction</span>
+                  </v-list-item>
+                </v-list>
+        </v-col>
+        </v-row>
+
+
+    </v-card-text>
+</v-card>
+</template>
